a298fa6f4cada2f20e3fb227a6b129158b5165f2
klinelozada <klinelozada@gmail.com>
2023-10-07 00:40:50 +0800

    update shortcode and style

--- js/rp.app.js
+++ js/rp.app.js
@@ -5,13 +5,37 @@ $(document).ready(function() {
 
     // Iterate over each .review-product-container element
     $(".review-product-container").each(function(index) {
+
+        // Add id on loop
+        $(this).find('.rp-score').attr('id', 'rpq' + (index + 1));
+        
         // Update the .total-no span with the total count
         $(this).find(".total-no").text(containerCount);
 
         // Update the .win-no span with the current index + 1
         $(this).find(".win-no").text(index + 1);
+
+        // Add circleProgress for rating
+        var item_rating = $(this).data('rating') * 0.1;
+        if(!item_rating){
+            item_rating = 0.25;
+        }
+
+        var progressBarOptions = {
+            startAngle: -1.55,
+            size: 90,
+            value: item_rating * 1,
+            fill: {
+                color: "#019309"
+            }
+        }
+
+        $(this).find('.rp-score').circleProgress(progressBarOptions).on('circle-animation-progress', function(event, progress, stepValue) {
+            $(this).find('.rp-score .score').text(String(stepValue.toFixed(2)));
+        });
+
     });
-    
+
     $(".rp-star-rating").each(function() {
         var $this = $(this);
         var ratingScore = $this.data("rating-score");
@@ -26,11 +50,8 @@ $(document).ready(function() {
             strokeWidth: 0,
             useGradient: false,
             minRating: 1,
-            readOnly: true,
-            callback: function(currentRating, $el) {
-                alert("rated " + currentRating);
-                console.log("DOM element ", $el);
-            }
+            readOnly: true
         });
     });
+
 });
